4bce6f1a892ae81067fa1466c85d346ef2b1debf
coreacathrine <146171629+1120coreacath@users.noreply.github.com>
2024-12-15 22:34:02 -0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
   define('DB_SERVER', 'localhost');
   define('DB_USERNAME', 'cath');
   define('DB_PASSWORD', '1234');
-  define('DB_NAME', 'student');
+  define('DB_NAME', 'result');
 
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
